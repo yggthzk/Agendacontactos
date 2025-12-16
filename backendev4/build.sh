9832977b0444fb2ed6a31aebe8f9ee7cbb7eba7e
bgrey77 <bgreyesq@gmail.com>
2025-12-15 21:26:19 -0300

    corrigiendo build.sh script render

--- backendev4/build.sh
+++ backendev4/build.sh
@@ -1,15 +1,11 @@
-#comportamiento render
 set -o errexit
 
-#dependencias del rpyecto
 pip install -r requirements.txt
-
 
 python manage.py collectstatic --no-input
 
-
 python manage.py migrate
-#creando el super user altiro pq sino pide suscripcion de pago
+
 export DJANGO_SUPERUSER_USERNAME=Administrador
 export DJANGO_SUPERUSER_EMAIL=admin@admin.com
 export DJANGO_SUPERUSER_PASSWORD=Artotska157
